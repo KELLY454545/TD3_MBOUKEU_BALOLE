2bd71607baf072d60e64cdaa6087b03f1a0bf55b
KELLY <kimpopico10@gmail.com>
2020-07-07 03:32:22 -0400

    ajout mulTowlnt.py

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -2,13 +2,23 @@ import sys
 
 print(sys.argv)
 
-x = int(sys.argv[1])
-y = int(sys.argv[2])
-
 if(len(sys.argv)==3):
-   print(x+y)
+    x = int(sys.argv[1])
+    y =int(sys.argv[2])
+    print(x+y)
+elif(len(sys.argv)==2):
+    x = int(sys.argv[1])
+    y=(input("veuillez entree la troixieme valeur:  "))
+    print(x+y)
+elif(len(sys.argv)==1):
+    x=(input("veuillez entree la deuxieme valeur: "))
+    y=(input("veuillez entree la troixieme valeur: "))
+    print(x+y)
+
 else:
     print("erreur")
+    
+
 
 
 
